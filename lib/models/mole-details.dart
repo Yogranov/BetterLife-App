5aede07aa8cf17c5d0a3228ae9fd190177a735a3
Yogi <frizen700@gmail.com>
2020-07-03 17:01:37 +0300

    fix the reloading issue on the homepage

--- lib/models/mole-details.dart
+++ lib/models/mole-details.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 
 class MoleDetails {
 
@@ -19,4 +20,38 @@ class MoleDetails {
 
 
   MoleDetails({this.imgUrl, this.img, this.figure, this.surface, this.size, this.color, this.benignPred, this.malignantPred, this.createTime, this.doctor, this.diagnosis, this.riskLevel, this.diagnosisCreateTime});
+
+  MoleDetails.fromJson(Map<String, dynamic> json)
+      : imgUrl = json['imgUrl'],
+        img = json['img'],
+        figure = json['figure'],
+        surface = json['surface'],
+        size = json['size'],
+        color = json['color'],
+        benignPred = json['benignPred'],
+        malignantPred = json['malignantPred'],
+        createTime = json['createTime'],
+        doctor = json['doctor'],
+        diagnosis = json['diagnosis'],
+        riskLevel = json['figure'],
+        diagnosisCreateTime = json['diagnosisCreateTime'];
+
+  Map<String, dynamic> toJson() => {
+        'imgUrl': imgUrl,
+        'img': img.toString(),
+        'figure': figure.toString(),
+        'surface': surface.toString(),
+        'size': size,
+        'color': color,
+        'benignPred': benignPred,
+        'malignantPred': malignantPred,
+        'createTime':  DateFormat('y-MM-d H:m:ss').format(createTime),
+        'doctor': doctor,
+        'diagnosis': diagnosis,
+        'riskLevel': riskLevel,
+        'diagnosisCreateTime': DateFormat('y-MM-d H:m:ss').format(diagnosisCreateTime),
+      };
+
+
+
 }
--- lib/models/mole.dart
+++ lib/models/mole.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:betterlife/models/mole-details.dart';
+import 'package:intl/intl.dart';
 
 class Mole {
   
@@ -9,4 +12,25 @@ class Mole {
 
   Mole({this.id, this.location, this.createTime, this.details});
 
+  Mole.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        location = json['location'],
+        createTime = json['createTime'],
+        details = json['details'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'location': location,
+        'createTime': DateFormat('y-MM-d H:m:ss').format(createTime),
+        'details': this.detailsToJsonList()
+      };
+
+  List<Map<String, dynamic>> detailsToJsonList() {
+    List<Map<String, dynamic>> allDetails = [];
+    this.details.map((detail) {
+      print(detail.color);
+      allDetails.add(detail.toJson());
+    });
+    return allDetails;
+  }
 }
